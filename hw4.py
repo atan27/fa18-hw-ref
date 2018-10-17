ffc81938e058655b48fa48dd29d9ea6d5e00543d
atan27 <atan27@illinois.edu>
2018-10-16 22:38:48 -0500

    finished code for most_common_char, alive_people, and alphabet_finder.

--- hw4.py
+++ hw4.py
@@ -7,13 +7,41 @@ You might find certain default Python packages immensely helpful.
 
 # Good luck!
 
+from collections import Counter
+
 """
 most_common_char
 
 Given an input string s, return the most common character in s.
 """
 def most_common_char(s):
-	pass
+    if s == "":
+        return "error: empty string does not have any characters"
+    else:
+        dict = {}
+        common = Counter(s).most_common(len(s))
+        for elem in common:
+            dict[elem[0]] = elem[1]
+        # print(dict)
+
+        #  a list of the # of occurrences of chars in string in order of greatest occurrence to least occurrence
+        lis_of_occurrences = []
+        for y in dict.items():
+            lis_of_occurrences.append(y[1])
+        # print(lis_of_occurrences)
+
+        # finds the max # of occurrence in lis_of_occurrences
+        maxNum = lis_of_occurrences[0]
+
+        # a list of the most common chars in string --> returns any one of the elements in this list
+        lis_of_common_char = []
+        for x in dict:
+            if dict[x] == maxNum:
+                lis_of_common_char.append(x)
+        # print(lis_of_common_char)
+
+        # returns most common char in string
+        return lis_of_common_char[0]
 
 
 """
@@ -25,19 +53,53 @@ If there is no such prefix, return None.
 Your function should recognize letters in both cases, i.e. "qwertyuiopASDFGHJKLzxcvbnm" is a valid alphabet.
 
 Example 1:
-	Argument:
-		"qwertyuiopASDFGHJKLzxcvbnm insensitive paella"
-	Return:
-		"qwertyuiopASDFGHJKLzxcvbnm"
+    Argument:
+        "qwertyuiopASDFGHJKLzxcvbnm insensitive paella"
+    Return:
+        "qwertyuiopASDFGHJKLzxcvbnm"
 
 Example 2:
-	Argument:
-		"aardvarks are cool!"
-	Return:
-		None
+    Argument:
+        "aardvarks are cool!"
+    Return:
+        None
 """
 def alphabet_finder(s):
-	pass
+    # creates an alphabet dictionary with all values set to 0
+    alpha_dict = {}
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    for letter in alphabet:
+        alpha_dict[letter] = 0
+    # print(alpha_dict)
+
+    # makes all letters in the string lowercase
+    s = s.lower()
+    # print(s)
+
+    # creates a list of all possible prefixes of the string
+    prefix_lis = s.split()
+    # print(prefix_lis)
+
+    # creates a list of all the prefixes that has 26 letters (alphabet has 26 letters)
+    valid_prefixes = []
+    for elem in prefix_lis:
+        if len(elem) == 26:
+            valid_prefixes.append(elem)
+    # print(valid_prefixes)
+
+    # returns shortest prefix that contains all letters of the alphabet only once. if none exist, returns None
+    for elem in valid_prefixes:
+        for letter in elem:
+            alpha_dict[letter] = 0
+            alpha_dict[letter] += 1
+        # print(alpha_dict)
+        if all(value == 1 for value in alpha_dict.values()) == True:
+            return elem
+        else:
+            continue
+    # print(alpha_dict)
+    if all(value == 0 for value in alpha_dict.values()) == True:
+        return None
 
 
 """
@@ -50,13 +112,13 @@ and b must be the largest value possible for the array.
 If multiple such subarrays exist (i.e. same b, different a), use the lowest value of a.
 
 Example:
-	Argument:
-		[1, 2, 3, 1, 4, 5, 6]
-	Return:
-		[1, 6]
+    Argument:
+        [1, 2, 3, 1, 4, 5, 6]
+    Return:
+        [1, 6]
 """
 def longest_unique_subarray(arr):
-	pass
+    pass
 
 
 """
@@ -76,30 +138,36 @@ Ungraded food for thought:
 Ideally, your method should also work on integer arrays without any modification.
 
 Example 1:
-	Argument:
-		"abcbabcdcdda"
-		There are 3 a's, 3 b's, 3 c's, and 3 d's. That means it is a very likable string!
-	Return:
-		True
+    Argument:
+        "abcbabcdcdda"
+        There are 3 a's, 3 b's, 3 c's, and 3 d's. That means it is a very likable string!
+    Return:
+        True
 
 Example 2:
-	Argument:
-		"aaabbbcccddde"
-		There are 3 a's, 3 b's, 3 c's, and 3 d's. We have 1 e, which we can remove.
-	Return:
-		True
+    Argument:
+        "aaabbbcccddde"
+        There are 3 a's, 3 b's, 3 c's, and 3 d's. We have 1 e, which we can remove.
+    Return:
+        True
 
 Example 3:
-	Argument:
-		"aaabbbcccdddeeffgg"
-		This string is similar to the other ones, except with 2 e's, f's and g's at the end.
-		To make this string likable, we need to remove the 2 e's, f's, and g's or we can remove
-		one a, b, c, and d. However all of these require more than one removal, so it becomes invalid.
-	Return:
-		False
+    Argument:
+        "aaabbbcccdddeeffgg"
+        This string is similar to the other ones, except with 2 e's, f's and g's at the end.
+        To make this string likable, we need to remove the 2 e's, f's, and g's or we can remove
+        one a, b, c, and d. However all of these require more than one removal, so it becomes invalid.
+    Return:
+        False
 """
 def string_my_one_true_love(s):
-	pass
+    cnt = Counter()
+    for letter in s:
+        cnt[letter] += 1
+    dict1 = {}
+    for x, y in cnt.items():
+        dict1[x] = y
+    print(dict1)
 
 
 """
@@ -111,13 +179,43 @@ Given that data, return the year where the most people represented in the list w
 If there are multiple such years, return the earliest year.
 
 Example:
-	Argument:
-		[[1920, 80], [1940, 22], [1961, 10]]
-	Return:
-		1961
+    Argument:
+        [[1920, 80], [1940, 22], [1961, 10]]
+    Return:
+        1961
 """
 def alive_people(data):
-	pass
+    dict2 = {}
+    for elem in data:
+        dict2[elem[0]] = elem[0] + elem[1]
+    # print(dict2)
+
+    # find the max of the birth year, then the min of the death year. the answer is between those values
+
+    # max of birth year
+    birth_year = []
+    for x in dict2.items():
+        birth_year.append(x[0])
+    # print(birth_year)
+    birth_year.sort(key=int, reverse=True)
+    # print(birth_year)
+    max_birth_year = birth_year[0]
+    #mprint(max_birth_year)
+
+    # min death year
+    death_year = []
+    for y in dict2.values():
+        death_year.append(y)
+    # print(death_year)
+    death_year.sort()
+    # print(death_year)
+    min_death_year = death_year[0]
+    # print(min_death_year)
+
+    if max_birth_year == min_death_year:
+        return "invalid"
+    else:
+        return max_birth_year
 
 
 """
@@ -131,16 +229,16 @@ Each inner list should be a triplet as we defined above.
 We don't care about the order of triplets, nor the order of elements in each triplet.
 
 Example:
-	Arguments:
-		[-1, 0, 1, 2, -1, -4], 0
-	Return:
-		[
-			[-1, 0, 1],
-			[-1, -1, 2]
-		]
+    Arguments:
+        [-1, 0, 1, 2, -1, -4], 0
+    Return:
+        [
+            [-1, 0, 1],
+            [-1, -1, 2]
+        ]
 """
 def three_sum(arr, t):
-	pass
+    pass
 
 
 """
@@ -150,19 +248,19 @@ Given an input integer n > 0, return the number of happy integers between 1 and 
 https://en.wikipedia.org/wiki/Happy_number
 
 Example 1:
-	Argument:
-		8
-		The happy numbers between 1 and 8 are 1 and 7 (7 -> 49 -> 97 -> 130 -> 10 -> 1)
-	Return:
-		2468 // 1234 (i.e., 2)
+    Argument:
+        8
+        The happy numbers between 1 and 8 are 1 and 7 (7 -> 49 -> 97 -> 130 -> 10 -> 1)
+    Return:
+        2468 // 1234 (i.e., 2)
 Example 2:
-	Argument:
-		15
-	Return:
-		4294967296 ** (1 / 16) (i.e., 4)
+    Argument:
+        15
+    Return:
+        4294967296 ** (1 / 16) (i.e., 4)
 """
 def happy_numbers(n):
-	pass
+    pass
 
 
 """
@@ -180,20 +278,21 @@ Ungraded food for thought:
 Think about how to generalize your solution to any arbitrary target sum.
 
 Example 1:
-	Argument:
-		[0, 1, 2, 3, 4, 5]
-		Clearly, the first element by itself forms a subarray with sum == 0.
-	Return:
-		[0, 1]
+    Argument:
+        [0, 1, 2, 3, 4, 5]
+        Clearly, the first element by itself forms a subarray with sum == 0.
+    Return:
+        [0, 1]
 
 Example 2:
-	Argument:
-		[10, 20, -20, 3, 21, 2, -6]
-		In this case, arr[1:3] = [20, -20], so there is a zero sum subarray.
-	Return:
-		[1, 2]
+    Argument:
+        [10, 20, -20, 3, 21, 2, -6]
+        In this case, arr[1:3] = [20, -20], so there is a zero sum subarray.
+    Return:
+        [1, 2]
 """
 def zero_sum_subarray(arr):
-    pass 
+    pass
+
 
 
